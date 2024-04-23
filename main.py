d162667481b88afedc7608e4d85940693763cce9
nick <deliciousman99@gmail.com>
2024-04-24 03:49:04 +0900

    added a platform column

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import os
+from typing import List
+
 from bs4 import BeautifulSoup
 import requests
 
 from models import Game, session, Base, engine
 
-prices_list = []
-games_list = []
-links_list = []
-
 game_name = input("Введите название игры: ")
 game_name_encode = game_name.replace(" ", "+")
 
 URL = f"https://psprices.com/region-tr/games/?q={game_name_encode}&content_type="
-
-print(URL)
 
 content = requests.get(URL)
 soup = BeautifulSoup(content.content, 'html.parser')
@@ -22,10 +18,33 @@ games = soup.find_all('span',
                       {'class': "leading-5 line-clamp-2 underline-offset-2 group-hover:underline"})
 prices = soup.find_all('span', {'class': 'font-bold'})
 
+platforms = soup.find_all('span', {'class': "flex items-center gap-1"})
+
+
+def get_platforms():
+    platforms_list = []
+    for i in platforms:
+        platform_arrays = f"{str(i.text)}\n"
+        sorted_list = []
+        split_list = platform_arrays.split(" ")
+
+        for element in split_list:
+            new_element = (element.replace('\n\n', '').
+                           replace('\n', ' ').replace('+E', ''))
+            sorted_list.append(new_element)
+        platforms_list.append(sorted_list)
+
+    cleaned_list = [[element.strip() for element in sublist] for sublist in platforms_list]
+    result_list = [' '.join(inner_list) for inner_list in cleaned_list]
+
+    return result_list
+
+
 Base.metadata.create_all(engine)
 
 
-def get_prices():
+def get_prices() -> List:
+    prices_list = []
     for price in prices:
         if 'bg-premium' not in price.get('class'):
             price_without_spaces = price.text.replace(" ", '').replace("\n", '')
@@ -34,48 +53,42 @@ def get_prices():
             for i in decode_price:
                 if i.startswith("₺"):
                     prices_list.append(i)
+    return prices_list
 
 
 def get_games():
+    games_list = []
     for game in games:
         games_list.append(game.text)
+    return games_list
 
 
 def get_links():
+    links_list = []
     for link in links:
         str_link = str(link["href"])
         if str_link.startswith("/region-tr/game/"):
             links_list.append(f"https://psprices.com{str_link}")
+    return links_list
 
 
 def create_new_list():
-    get_games()
-    get_prices()
-    get_links()
-    new_list = list(zip(games_list, prices_list, links_list))
+    new_list = list(zip(get_games(), get_platforms(),
+                        get_prices(), get_links()))
     return new_list
-
-
-def generate_result_filename():
-    i = 1
-    while os.path.exists(f"result_{i}.txt"):
-        i += 1
-    return f"result_{i}.txt"
-
-
-filename = generate_result_filename()
 
 
 def insert_into_db():
     for i in create_new_list():
         new_games = Game(game_name=i[0],
-                         price=i[1],
-                         link=i[2])
+                         platform=i[1],
+                         price=i[2],
+                         link=i[3])
 
         session.add(new_games)
         session.commit()
-        with open(filename, "a") as file:
-            file.write(f"{i[0]}, {i[1]}\n{i[2]}\n\n")
+        with open(f"result_{game_name}", "a") as file:
+            file.write(f"{i[0]}, {i[1]}, {i[2]}\n{i[3]}\n\n")
 
 
 insert_into_db()
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Integer, Column, String, Date, Float, Boolean, DateTime
+from sqlalchemy import create_engine, Integer, Column, String, Date, Float, Boolean, DateTime, ARRAY
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -16,5 +16,6 @@ class Game(Base):
     __tablename__ = 'Games'
     id = Column(Integer, primary_key=True, index=True)
     game_name = Column(String, index=True)
+    platform = Column(String, index=True)
     price = Column(String, index=True)
     link = Column(String, index=True)
